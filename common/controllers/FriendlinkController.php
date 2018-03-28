bd3aa890d0bc9f4f663d0cf18b09c824538b7a34
你是我的XXX <kmdgs@qq.com>
2018-03-28 15:59:37 +0800

    友情链接

--- common/controllers/FriendlinkController.php
+++ common/controllers/FriendlinkController.php
@@ -9,7 +9,38 @@
 namespace api\common\controllers;
 
 
+use api\common\models\ApiFriendLink;
+use Yii;
+use yii\base\DynamicModel;
+
 class FriendlinkController extends ApiTokenController
 {
+
+    /**
+     * 获取友情链接
+     * @author 黄东 kmdgs@qq.com
+     * @return array
+     */
+    public function actions()
+    {
+        $actions = parent::actions();
+        $requestParams = Yii::$app->request->queryParams;
+
+        $actions['index'] = [
+            'class' => 'yii\rest\IndexAction',
+            'modelClass' => $this->modelClass,
+            'checkAccess' => [$this, 'checkAccess'],
+            'dataFilter' => [
+                'class' => 'yii\data\ActiveDataFilter',
+                'searchModel' => function () {
+                    return (new DynamicModel(['type' => null]))
+                        ->addRule('type', 'integer');
+                },
+                'filter' => ApiFriendLink::getFilterParams($requestParams),
+            ]
+        ];
+        return $actions;
+    }
+
     public $modelClass = 'api\common\models\ApiFriendLink';
 }
--- common/models/ApiFriendLink.php
+++ common/models/ApiFriendLink.php
@@ -12,5 +12,23 @@ use common\models\module\Friendlink;
 
 class ApiFriendLink extends Friendlink
 {
+    /**
+     * 根据传递参数生成查询过滤条件
+     * @author 黄东 kmdgs@qq.com
+     * @param $requestParams
+     * @return array
+     */
+    public static function getFilterParams($requestParams)
+    {
+        $params_eq = ['type'];
+        $filter = [];
+        //等于查询条件
+        foreach ($params_eq as $value) {
+            if (!empty($requestParams[$value])) {
+                $filter[$value] = $requestParams[$value];
+            }
+        }
+        return $filter;
+    }
 
 }
